3d3283266aacaa5e3b0fb0a76f501f15188f0a55
sobia-shah <sobiy58@gmail.com>
2024-10-05 00:30:55 +0500

    Add files via upload

---
+++ assigmnt5 chap 3to 9/aap.js
@@ -0,0 +1,203 @@
+           //chapter 3.Variables for Numbers
+//Question number 1
+// var height = 170;
+// var newHeight = height + 10;
+// console.log(newHeight);
+
+//Question number 2
+// var num1 = 50;
+// var num2 = 30;
+// var sum = num1 + num2;
+// alert(sum);
+
+//Question number 3
+// var numToBeAdded = 20;
+//var total= 0;
+//total += numToBeAdded
+// alert(total)
+
+
+//Question number 4
+// var price = 120;
+// var tax = price * 0.10;
+// var totalPrice = price + tax;
+// alert(totalPrice);
+
+
+                    //4.Variables for Numbers
+//Question number 1
+// var myAdress = "karachi";
+// alert(myAdress);                 
+
+//Question number 2
+// var userName = "Sobia Naz";
+// var userEmail = "sobiy58@gmail.com";
+// var userPassword = 321456;
+// alert("Username: " + userName + "\nEmail: " + userEmail + "\nPassword: " + userPassword);
+
+//Question number 3
+// var user address = "Malir Kalaboard";//incurrect
+// var userAdress = "Malir Kalaboard"
+// alert(userAdress)
+
+
+//Question number 4
+// var rose = "Floribundas";
+// var Rose = "Floribundas";
+// alert(rose+" "+Rose);
+
+                       //5.Math expressions:Familiar operators
+//Question number 1
+// var num1 = 25;
+// var num2 = 30;
+// var sum = (num1 + num2);
+// alert(sum);
+
+//Question number 2
+// var num1 = 25;
+// var num2 = 5;
+// var sum = (num1 + num2);
+// alert(sum);
+
+//Question number 3 
+//var modulusResult = 27 % 4;
+//alert(modulusResult);   
+
+
+                        //6.Math expressions:Unfamiliar operators
+//Question number 1
+// var num = 3;
+// var newNum = num++;
+// alert("Num" + num);
+// alert("New Num" + newNum);
+
+//Question number 2
+// var counter = 10;
+// counter++;
+// counter++;
+// alert(counter);
+
+//Question number 3
+// var points = 20;
+// points--;
+// ++points;
+// alert(points);
+
+
+//Question number 4
+// var number = 0;
+// ++number;
+// ++number;
+// ++number;
+// alert(number);
+
+                       //7.Math expressions:Eliminating ambiguity
+//Question number 1
+//var calculation = 20 % 6;
+//carlculation = 2
+
+
+//Question number 2
+//var expressionValue = (6 + 2)*(5 - 3);
+
+//                      =  8   *   2;
+//                      =      16;
+
+//Question number 3
+//Parentheses () have the highest precedence and are evaluated first.
+//Exponents ** come next.
+//Multiplication *, Division /, and Modulus % have the same precedence and are evaluated from left to right.
+//Addition + and Subtraction - also have the same precedence and are evaluated from left to right
+
+//Question number 4 
+//question samjh nah aya     
+
+
+
+
+                        //8.Prompts
+
+//Question number 1
+//var userName = prompt("What is your name?");
+//alert("Hello, " + userName + "!")
+
+//Question number 2
+// var age = prompt("How old are you?");
+
+// if (age >=1) {
+//    alert("You are " + age + " years old.");
+// } else {
+//    alert("No age provided");
+// }
+
+//Question number 3
+// var pets = prompt("How many pets do you have?");
+// if (pets >=1) {
+//    alert("You have " + pets + " pets");
+// } else {
+//    alert("You didn't enter anything.");
+// }
+
+//Question number 4
+// var userInput = prompt("Enter a number between 1 and 10");
+// var number = Number(userInput);
+// var result = number * 2;
+// alert("The result is: " + result);
+
+//Question number 5  
+// var favNum = prompt("What is your favorite number?");
+
+// if (favNum === "") {
+//     favNum = 7; 
+// }
+// alert("Your favorite number is: " + favNum);                      
+
+
+                           //9.if statements
+//Question number 1                             
+// var capital = prompt("What is the capital of France?");
+
+// if (capital === "paris") {
+//     alert("Correct!");
+// } else {
+//     alert("Try again!");
+// }                           
+
+//Question number 2 
+// var capital = prompt("What is the capital of France?");
+
+// if (capital === "paris") {
+//     alert("Correct!");
+// } else {
+//     alert("Incorrect, the correct answer is Paris.");
+// }
+
+//Question number 3 
+// var capital = prompt("What is the capital of the United Kingdom?");
+
+// if (capital === "london" || capital === "the united kingdom") {
+//     alert("Correct!");
+// } else {
+//     alert("Incorrect, the correct answer is London.");
+// }
+
+
+//Question number 4 
+
+
+
+
+
+//Question number 5  
+// var capital = prompt("What is the capital of Germany?");
+// var number = +prompt("What is 10 + 10?");
+
+// if (capital == correct_answers9("berlin"))
+// correct_count += 1
+
+// else if (number == correct_answers(20))
+// correct_count += 1
+
+// else if (correct_count == 2){"You got both right!"}
+// else if (correct_count == 1){"You got one correct!"}
+// else{"You got none correct."}
---
+++ assignmnt5 chap1 to 2/aap.js
@@ -0,0 +1,46 @@
+               //Chapter number 1 ALerts
+//question number 1...
+//Alert is pops up to give the user a message..
+
+//question number 2...
+// alert("Welcome to my website!.");
+
+//question number 3...
+//If you capitalize it, the script will stop.
+
+//question number 4...
+// alert("Hello, world!");
+
+//question number 5...
+//alert("write somthing"); //in codding the quted text is string
+//the opening parenthesis is jammed up against the keyword, 
+
+            //Chapter number  2.Variables for Strings
+
+//question number 1...
+// var  massage = "Hello, world!";
+// alert(massage);
+
+
+//question number 2...
+// var age;
+// age = 25;
+// alert(age);
+
+//question number 3...
+// var greeting = "Good morning!";
+
+// alert(greeting);
+
+// greeting = "Good evening!";
+
+// alert(greeting);
+
+// //question number 4...
+// var favoriteColor = "blue";
+// alert("My favorite color is " + favoriteColor);
+
+// //question number 5...
+// var age;
+// age = 25;
+// alert(age);
